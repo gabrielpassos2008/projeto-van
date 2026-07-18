a2051d55b5376d023356dee57dede3ef06e1c49a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-18 15:22:08 -0300

    primeiro commit

---
+++ src/main/java/com/gabriel/projeto_van/ProjetoVanApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.projeto_van;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoVanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoVanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gabriel/projeto_van/ProjetoVanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.projeto_van;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoVanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
