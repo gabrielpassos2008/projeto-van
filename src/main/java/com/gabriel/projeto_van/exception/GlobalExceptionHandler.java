3f6927d8dee7e28143b28bc30454a5aeefa486e8
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-20 20:32:46 -0300

    - criando meu handler. - adicionando EmailJaExistenteExpeption na handler junto com o objeto de resposta

---
+++ src/main/java/com/gabriel/projeto_van/exception/GlobalExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.gabriel.projeto_van.exception;
+
+import com.gabriel.projeto_van.exception.exceptions.EmailJaExistenteException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(EmailJaExistenteException.class)
+    public ResponseEntity<MensagemErroDTO> emailJaExistente(EmailJaExistenteException excecao){
+        MensagemErroDTO erro = new MensagemErroDTO(
+                HttpStatus.CONFLICT.value(),
+                excecao.getMessage(),
+                HttpStatus.CONFLICT.name());
+
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(erro);
+    }
+}
