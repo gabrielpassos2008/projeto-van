c134bd66023ffa34ca55a2a0a3848ed02afc1ab1
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-19 22:01:10 -0300

    - adicionando a role do rele do Administrador.

--- src/main/java/com/gabriel/projeto_van/dto/administrador/AdministradorCreateDTO.java
+++ src/main/java/com/gabriel/projeto_van/dto/administrador/AdministradorCreateDTO.java
@@ -1,4 +1,6 @@
 package com.gabriel.projeto_van.dto.administrador;
 
-public record AdministradorCreateDTO(String email,String senha) {
+import com.gabriel.projeto_van.model.Role;
+
+public record AdministradorCreateDTO(String email, String senha, Role role) {
 }
