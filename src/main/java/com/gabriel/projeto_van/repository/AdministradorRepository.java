baf5235b438e6ad513f94de87e527b3b8199709a
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-21 20:12:11 -0300

    - adicionando uma query para retorna o Administrador pelo email

--- src/main/java/com/gabriel/projeto_van/repository/AdministradorRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/AdministradorRepository.java
@@ -3,5 +3,10 @@ package com.gabriel.projeto_van.repository;
 import com.gabriel.projeto_van.model.Administrador;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
+
+    Optional<Administrador> findByEmail(String email);
+
 }
