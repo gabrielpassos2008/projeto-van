c66e7b71f91dbb13cbaac657bd4e7b3e4721b657
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-23 20:04:07 -0300

    - adicionando o UsuarioLogin no registro de uma novo Cliente

--- src/main/java/com/gabriel/projeto_van/service/ClienteService.java
+++ src/main/java/com/gabriel/projeto_van/service/ClienteService.java
@@ -3,6 +3,7 @@ package com.gabriel.projeto_van.service;
 import com.gabriel.projeto_van.dto.cliente.ClienteCreateDTO;
 import com.gabriel.projeto_van.dto.cliente.ClienteReponseDTO;
 import com.gabriel.projeto_van.model.Cliente;
+import com.gabriel.projeto_van.model.UsuarioLogin;
 import com.gabriel.projeto_van.repository.ClienteRepository;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,13 +15,20 @@ public class ClienteService {
     @Autowired
     private ClienteRepository clienteRepository;
 
+    @Autowired
+    private UsuarioLoginService usuarioLoginService;
+
     public ClienteReponseDTO registrar (ClienteCreateDTO dto){
         Cliente cliente = new Cliente();
 
-        cliente.setEmail(dto.email());
-        cliente.setSenha(dto.senha());
+        
+        UsuarioLogin usuarioLogin = usuarioLoginService.registrar(dto.email(), dto.senha(),dto.role());
+
+        cliente.setEmail(usuarioLogin.getEmail());
+        cliente.setSenha(usuarioLogin.getSenha());
         cliente.setNome(dto.nome());
         cliente.setTelefone(dto.telefone());
+        cliente.setUsuarioLogin(usuarioLogin);
 
         this.clienteRepository.save(cliente);
         return new ClienteReponseDTO(cliente.getId(), cliente.getEmail(), cliente.getNome());
