eb76e21ca4a38f8078991b3a698493ed8e444c86
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-23 20:12:48 -0300

    - adicionando o novo query no repository. findByEmail

--- src/main/java/com/gabriel/projeto_van/repository/MotoristaRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/MotoristaRepository.java
@@ -3,5 +3,10 @@ package com.gabriel.projeto_van.repository;
 import com.gabriel.projeto_van.model.Motorista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MotoristaRepository extends JpaRepository<Motorista ,Long> {
+
+    Optional<Motorista> findByEmail(String email);
+
 }
