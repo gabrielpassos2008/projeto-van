78e07211f78554457ceb683611626c1c6d983a98
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-23 20:15:23 -0300

    - alteracoes no metodo registrar da service Cliente. - adicionando o cadastro do motorista como chave estrangeira. - criando o UsuarioLogin com os mesmo dados junto.

--- src/main/java/com/gabriel/projeto_van/service/ClienteService.java
+++ src/main/java/com/gabriel/projeto_van/service/ClienteService.java
@@ -3,9 +3,9 @@ package com.gabriel.projeto_van.service;
 import com.gabriel.projeto_van.dto.cliente.ClienteCreateDTO;
 import com.gabriel.projeto_van.dto.cliente.ClienteReponseDTO;
 import com.gabriel.projeto_van.model.Cliente;
+import com.gabriel.projeto_van.model.Motorista;
 import com.gabriel.projeto_van.model.UsuarioLogin;
 import com.gabriel.projeto_van.repository.ClienteRepository;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,16 +18,28 @@ public class ClienteService {
     @Autowired
     private UsuarioLoginService usuarioLoginService;
 
+    @Autowired
+    private MotoristaService motoristaService;
+
+
     public ClienteReponseDTO registrar (ClienteCreateDTO dto){
         Cliente cliente = new Cliente();
 
-        
+        String email = usuarioLoginService.retornarUsuarioPeloEmailDaAuntenticacao();
+        Motorista motorista = motoristaService.retornarMotoristaEmail(email);
+
+
         UsuarioLogin usuarioLogin = usuarioLoginService.registrar(dto.email(), dto.senha(),dto.role());
 
+        // adicionando os email e senha do UsuarioLogin
         cliente.setEmail(usuarioLogin.getEmail());
         cliente.setSenha(usuarioLogin.getSenha());
+        // adicionar os conteudo pelo dtos
         cliente.setNome(dto.nome());
         cliente.setTelefone(dto.telefone());
+
+        // adicionando as FK
+        cliente.setMotorista(motorista);
         cliente.setUsuarioLogin(usuarioLogin);
 
         this.clienteRepository.save(cliente);
