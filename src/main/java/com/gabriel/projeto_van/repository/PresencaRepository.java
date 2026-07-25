dae8e36c1d684edcc3c7aebdd0a84581223a0c3f
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-25 16:48:49 -0300

    - ajuste na prenscaRepository, antes o tinha colocado como base a prensacaRepository, sendo que tinha que ser a entity

--- src/main/java/com/gabriel/projeto_van/repository/PresencaRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/PresencaRepository.java
@@ -1,6 +1,7 @@
 package com.gabriel.projeto_van.repository;
 
+import com.gabriel.projeto_van.model.Presenca;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PresencaRepository extends JpaRepository<PresencaRepository,Long> {
+public interface PresencaRepository extends JpaRepository<Presenca,Long> {
 }
