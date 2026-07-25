d79fe2547cb5e67970af45a80193283e33c817a3
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-25 16:47:59 -0300

    - adicionando a exection na Handler e enviando o o dto do erro

--- src/main/java/com/gabriel/projeto_van/exception/GlobalExceptionHandler.java
+++ src/main/java/com/gabriel/projeto_van/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.gabriel.projeto_van.exception;
 
 import com.gabriel.projeto_van.dto.Exception.MensagemErroDTO;
 import com.gabriel.projeto_van.exception.exceptions.EmailJaExistenteException;
+import com.gabriel.projeto_van.exception.exceptions.TokenInvalidoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,14 +13,26 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EmailJaExistenteException.class)
-    public ResponseEntity<MensagemErroDTO> emailJaExistente(EmailJaExistenteException excecao){
+    public ResponseEntity<MensagemErroDTO> emailJaExistente(EmailJaExistenteException exception){
         MensagemErroDTO erro = new MensagemErroDTO(
                 HttpStatus.CONFLICT.value(),
-                excecao.getMessage(),
+                exception.getMessage(),
                 HttpStatus.CONFLICT.name());
 
         return ResponseEntity
-                .status(HttpStatus.NOT_FOUND)
+                .status(HttpStatus.CONFLICT)
+                .body(erro);
+    }
+
+    @ExceptionHandler(TokenInvalidoException.class)
+    public ResponseEntity<MensagemErroDTO> tokenInvalido (TokenInvalidoException exception){
+        MensagemErroDTO erro = new MensagemErroDTO(
+                HttpStatus.UNAUTHORIZED.value(),
+                exception.getMessage(),
+                HttpStatus.UNAUTHORIZED.name());
+
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
                 .body(erro);
     }
 }
