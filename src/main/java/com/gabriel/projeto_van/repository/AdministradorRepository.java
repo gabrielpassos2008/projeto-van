64f09409d0560bc7412051f0a85baa2797359426
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-28 17:08:04 -0300

    - Adiciona o método existsByEmail para validação do Email

--- src/main/java/com/gabriel/projeto_van/repository/AdministradorRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/AdministradorRepository.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 
 public interface AdministradorRepository extends JpaRepository<Administrador,Long> {
 
+    boolean existsByEmail(String email);
     Optional<Administrador> findByEmail(String email);
 
 }
