571e8112415f6c40d5b9a236914078d7f4c9f853
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-29 19:29:24 -0300

    - adiciona os @NotBlank para validar o DTO da autenticação.

--- src/main/java/com/gabriel/projeto_van/dto/auth/AutenticacaoDTO.java
+++ src/main/java/com/gabriel/projeto_van/dto/auth/AutenticacaoDTO.java
@@ -1,4 +1,10 @@
 package com.gabriel.projeto_van.dto.auth;
 
-public record AutenticacaoDTO(String email, String senha) {
+import jakarta.validation.constraints.NotBlank;
+
+public record AutenticacaoDTO(
+        @NotBlank(message = "Email é obrigatório")
+        String email,
+        @NotBlank(message = "Senha é obrigatório")
+        String senha) {
 }
