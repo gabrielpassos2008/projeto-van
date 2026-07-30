12ac4162b921de16b30610251d5321750fd3a432
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-30 10:59:25 -0300

    - cria o método existsByNomeAndMotorista e findByNomeAndMotorista no CorridaRepository.

--- src/main/java/com/gabriel/projeto_van/repository/CorridaRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/CorridaRepository.java
@@ -1,7 +1,14 @@
 package com.gabriel.projeto_van.repository;
 
 import com.gabriel.projeto_van.model.Corrida;
+import com.gabriel.projeto_van.model.Motorista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CorridaRepository extends JpaRepository<Corrida,Long> {
+
+    Optional<Corrida> findByNomeAndMotorista(String nome, Motorista motorista);
+
+    boolean existsByNomeAndMotorista(String nome, Motorista motorista);
 }
