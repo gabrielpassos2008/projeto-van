3db7235ac87934e53fd24792352a1f241ca0c9ce
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-07-31 22:46:23 -0300

    - Cria o novo meted que vai retornar os clientes cadastrados pelo motorista do parâmetro.

--- src/main/java/com/gabriel/projeto_van/repository/ClienteRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/ClienteRepository.java
@@ -1,10 +1,15 @@
 package com.gabriel.projeto_van.repository;
 
 import com.gabriel.projeto_van.model.Cliente;
+import com.gabriel.projeto_van.model.Motorista;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
 
     boolean existsByEmail(String email);
+
+    List<Cliente> findByMotorista(Motorista motorista);
 
 }
