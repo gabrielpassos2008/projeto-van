6add17d8ba48cc9952e7d2201916c54d4fb3c7c8
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-01 18:14:36 -0300

    - Cria o novo no ClienteRepository aonde ele vai retornas os clientes cadastrados pelo motorista e pelo nome que mandou de parâmetro

--- src/main/java/com/gabriel/projeto_van/repository/ClienteRepository.java
+++ src/main/java/com/gabriel/projeto_van/repository/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.gabriel.projeto_van.repository;
 
 import com.gabriel.projeto_van.model.Cliente;
+import com.gabriel.projeto_van.model.Corrida;
 import com.gabriel.projeto_van.model.Motorista;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,5 +12,9 @@ public interface ClienteRepository extends JpaRepository<Cliente,Long> {
     boolean existsByEmail(String email);
 
     List<Cliente> findByMotorista(Motorista motorista);
+    // Containing → busca por parte do nome (não precisa ser igual)
+    // IgnoreCase → ignora maiúsculas e minúsculas
+    List<Cliente> findByMotoristaAndNomeContainingIgnoreCase(Motorista motorista, String nome);
+
 
 }
