bad6241b9bf8165afb4a42104be6296a35d00634
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-05 16:19:13 -0300

    - cria uma nova rota post para registrar prenseças novas. Adicionada no ClienteController

--- src/main/java/com/gabriel/projeto_van/controller/ClienteController.java
+++ src/main/java/com/gabriel/projeto_van/controller/ClienteController.java
@@ -1,7 +1,29 @@
 package com.gabriel.projeto_van.controller;
 
+import com.gabriel.projeto_van.dto.presenca.PresencaCreateDTO;
+import com.gabriel.projeto_van.service.PresencaService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    private PresencaService presencaService;
+
+    @PostMapping("/registrar/presenca")
+    public ResponseEntity<String> postResgistrarPresenca (@RequestBody @Valid PresencaCreateDTO dto){
+        String mensagem = presencaService.criarPresensa(dto.corridaID());
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(mensagem);
+    }
 }
