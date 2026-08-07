2e03134ed8bfb29d382c751d67dd06062d70f8e5
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-07 16:13:19 -0300

    - cria nova rota para retornar corrida pelo cliente autenticado.

--- src/main/java/com/gabriel/projeto_van/controller/ClienteController.java
+++ src/main/java/com/gabriel/projeto_van/controller/ClienteController.java
@@ -1,15 +1,16 @@
 package com.gabriel.projeto_van.controller;
 
+import com.gabriel.projeto_van.dto.corrida.CorridaResponseDTO;
 import com.gabriel.projeto_van.dto.presenca.PresencaCreateDTO;
+import com.gabriel.projeto_van.service.CorridaService;
 import com.gabriel.projeto_van.service.PresencaService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/cliente")
@@ -18,6 +19,9 @@ public class ClienteController {
     @Autowired
     private PresencaService presencaService;
 
+    @Autowired
+    private CorridaService corridaService;
+
     @PostMapping("/registrar/presenca")
     public ResponseEntity<String> postResgistrarPresenca (@RequestBody @Valid PresencaCreateDTO dto){
         String mensagem = presencaService.criarPresensa(dto);
@@ -25,5 +29,13 @@ public class ClienteController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(mensagem);
+    }
+
+    @GetMapping("listar/corrida")
+    public ResponseEntity<List<CorridaResponseDTO>> getListarCorrida(){
+        List<CorridaResponseDTO> lista = corridaService.listarCorrida();
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(lista);
     }
 }
