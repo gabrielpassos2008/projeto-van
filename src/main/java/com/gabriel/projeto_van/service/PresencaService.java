92ffdcbac8136f6f31f667c59b2cc611ccfad7df
gabrielpassos2008 <gabrieldospa1109@gmail.com>
2026-08-07 20:28:42 -0300

    - cria novo metodo para validar o dia da prenseca.

--- src/main/java/com/gabriel/projeto_van/service/PresencaService.java
+++ src/main/java/com/gabriel/projeto_van/service/PresencaService.java
@@ -1,6 +1,7 @@
 package com.gabriel.projeto_van.service;
 
 import com.gabriel.projeto_van.dto.presenca.PresencaCreateDTO;
+import com.gabriel.projeto_van.exception.exceptions.PresencaJaRegistradaException;
 import com.gabriel.projeto_van.model.Cliente;
 import com.gabriel.projeto_van.model.Corrida;
 import com.gabriel.projeto_van.model.Motorista;
@@ -10,6 +11,7 @@ import com.gabriel.projeto_van.repository.PresencaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Service
@@ -27,23 +29,32 @@ public class PresencaService {
     @Autowired
     private ClienteService clienteService;
 
+    public void validarDiaPresenca(Corrida corrida, Cliente cliente){
+
+        LocalDate hoje = LocalDate.now();
+        // Define o início do dia atual (00:00:00)
+        LocalDateTime inicioDoDia = hoje.atStartOfDay();
+        // Define o início do próximo dia para delimitar o fim da busca
+        LocalDateTime inicioDoProximoDia = hoje.plusDays(1).atStartOfDay();
+        boolean existe = presencaRepository.existePresencaNoDia(cliente,corrida,inicioDoDia,inicioDoProximoDia);
+        if (existe){
+            throw new PresencaJaRegistradaException();
+        }
+    }
+
+
     public String criarPresensa(PresencaCreateDTO dto){
-        // validar se a já existe aquela corrida para o cliente na mesma data e no mesmo dia.
+        Corrida corrida = corridaService.retornarCorridaPorId(dto.corridaID());
+        Cliente cliente = clienteService.retornarClienteAutenticado();
+
+        validarDiaPresenca(corrida,cliente);
 
         Presenca presenca = new Presenca();
-
-        Corrida corrida = corridaService.retornarCorridaPorId(dto.corridaID());
-
-        Cliente cliente = clienteService.retornarClienteAutenticado();
-        Motorista motorista = motoristaService.retornarMotoristaAutenticado();
-
-        this.motoristaService.validarSeClientePertenceAoMotorisra(corrida,cliente);
-
 
         presenca.setStatus(dto.status());
         presenca.setDataHora(LocalDateTime.now());
         presenca.setCliente(cliente);
-        presenca.setMotorista(motorista);
+        presenca.setMotorista(cliente.getMotorista());
         presenca.setCorrida(corrida);
 
         this.presencaRepository.save(presenca);
